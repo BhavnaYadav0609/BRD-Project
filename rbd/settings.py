df1e3b90e03bf22a31c036b187ae9e230c06398b
FIRST REPOSITORY <bhaviyadav0609@gmail.com>
2025-10-28 20:37:24 +0530

    Updated settings.py

--- rbd/settings.py
+++ rbd/settings.py
@@ -79,13 +79,8 @@ WSGI_APPLICATION = 'rbd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NAME'),
-        'USER':env('DB_USER'),
-        'PASSWORD':env('DB_PASSWORD'),
-        'PORT':env('DB_PORT'),
-        'CONN_MAX_AGE':500,
-        'ATOMIC_REQUESTS':True,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3"
     }
 }
 
